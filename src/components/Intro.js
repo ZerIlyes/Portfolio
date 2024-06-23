a409e7e797e91550c3b6956d08705632701a363b
ZerIlyes <ilyes.zeroual@gmail.com>
2024-06-23 21:16:03 +0200

    fix

---
+++ src/components/Intro.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Intro = () => {
+    return (
+        <div className="container mt-5">
+            <div className="card">
+                <div className="card-body">
+                    <h1 className="card-title">Introduction</h1>
+                    <p className="card-text">
+                        Je m'appelle Zeroual Ilyes et je suis actuellement en troisième année de BUT Informatique. Mon choix de m'orienter vers cette filière découle de ma passion pour le développement, une passion que j'entretiens depuis mon plus jeune âge. J'ai toujours rêvé de créer mes propres projets et de concevoir des solutions innovantes.
+                    </p>
+                    <p className="card-text">
+                        Mon parcours a commencé par un lycée général, où j'ai suivi une spécialité en Numérique et Sciences Informatiques. Cette orientation m'a permis de développer une solide base en informatique et de confirmer mon intérêt pour ce domaine. Après le lycée, j'ai décidé de poursuivre mes études en BUT Informatique à Villetaneuse, où j'ai eu l'occasion de travailler sur divers projets pratiques et d'approfondir mes connaissances théoriques.
+                    </p>
+                    <p className="card-text">
+                        Pour la suite de mon parcours, je souhaite continuer mes études à l'École d'Ingénieurs ESIEE en apprentissage. Mon objectif est de me spécialiser dans le développement web, un domaine qui m'attire particulièrement pour ses possibilités créatives et son dynamisme.
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Intro;
